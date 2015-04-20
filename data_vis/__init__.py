582d19d210c27f4b4fdb24937c0b9197c8b21cee
Thomas Cranny <Thom@sCranny.net>
2015-04-20 22:03:11 +1000

    Library Changes

--- data_vis/__init__.py
+++ data_vis/__init__.py
@@ -8,8 +8,14 @@
 """
 
 from flask import Flask
+from flask.ext import restful
+
+from api import Course
 
 app = Flask(__name__)
+api = restful.Api(app)
+
+api.add_resource(Course, '/api/course/<string:course_code>')
 
 
 from . import views
---
+++ data_vis/api.py
@@ -0,0 +1,27 @@
+"""
+    API
+    ===
+
+    Provides the classes used for providing a HTTP JSON API.
+
+"""
+
+import re
+
+from flask.ext.restful import abort, Resource
+
+class Course(Resource):
+    """
+    """
+
+    def get(self, course_code):
+        """ Return information in JSON form about a UQ Course.
+        """
+        if not re.match('^[a-z]{4}[0-9]{4}$', course_code):
+            abort(404, message='Not a valid course code')
+
+
+        return {
+            'name': 'not known',
+            'code': course_code,
+        }
